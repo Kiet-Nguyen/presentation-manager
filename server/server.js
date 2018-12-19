9c4fc64280cca6818dacf8f7db3a6ce1b621f44e
Kiet <bunbothomngon@gmail.com>
2018-12-19 12:49:29 +0200

    Add path in server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const { mongoose } = require('./db/mongoose');
 const { Presentation } = require('./models/presentation');
@@ -38,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
-  app.get('*', function (req, res) {
+  app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
